c19c2c87178fe5d5f1680b5ebdc1f906f3d02d2f
Jonathan Flatt <jonathanf@demandchain.com>
2021-12-04 18:28:49 -0600

    move args to table?

--- lua/nvim-sfdx/init.lua
+++ lua/nvim-sfdx/init.lua
@@ -10,43 +10,63 @@ local function i(obj)
   print(vim.inspect(obj))
 end
 
-local function deploy(metadata, username)
-  local cmd = 'sfdx force:source:deploy '
-  if (metadata~=nil)
-    then
-      cmd = cmd .. ' -m ' .. metadata ..  ' -l RunSpecifiedTests -r SiteTests'
-    else
-      local wd = vim.fn.getcwd()
-      cmd = cmd .. ' -x ' .. wd .. '/package.xml'
+local function run_sfdx(cmd, ...)
+  local cmd_string = 'sfdx ' .. cmd
+  for _,v in pairs({...}) do
+    cmd_string = cmd_string .. ' ' .. v
   end
-  if (username~=nil)
-    then
-      cmd = cmd .. ' -u ' .. username
-  end
-
-  cmd = cmd .. ' --json'
-
-  local handle = io.popen(cmd)
-  local result = handle:read("*a")
-  local deserialized = vim.fn.json_decode(result)
-  -- i(deserialized)
-  local deploymentResponse = deserialized.result.deployedSource
-  local qflist = {}
-  for _, v in ipairs(deploymentResponse)
-    do
-    if v.state =='Failed'
-      then
-      local qf = {filename = v.filePath, lnum = v.lineNumber, col = v.columnNumber, text = v.error }
-      table.insert(qflist, qf)
-    end
-  end
-  if next(qflist) ~= nil
-    then
-    vim.fn.setqflist(qflist)
-    vim.api.nvim_command("botright copen")
-  end
-  handle.close()
 end
+
+local sfdx_project_root = require("lspconfig/util").root_pattern('sfdx-project.json', '.git')
+i(sfdx_project_root)
+-- local function get_default_username()
+--   local file = sfdx_project_root 
+-- local default_username = io.read
+
+local function deploy(args)
+  local default_username = 'defaultUsername'
+  local cmd = 'force:source:deploy'
+  local default_args = {
+    x = project_root .. '/.package.xml',
+    u = default_username
+  }
+  run_sfdx(cmd, args)
+  -- if (metadata~=nil)
+ --   then
+  --     cmd = cmd .. ' -m ' .. metadata ..  ' -l RunSpecifiedTests -r SiteTests'
+  --   else
+  --     local wd = vim.fn.getcwd()
+  --     cmd = cmd .. ' -x ' .. wd .. '/package.xml'
+  -- end
+  -- if (username~=nil)
+  --   then
+  --     cmd = cmd .. ' -u ' .. username
+  -- end
+  -- cmd = cmd .. ' --json'
+
+  -- local handle = io.popen(cmd)
+  -- local result = handle:read("*a")
+  -- local deserialized = vim.fn.json_decode(result)
+  -- -- i(deserialized)
+  -- local deploymentResponse = deserialized.result.deployedSource
+  -- local qflist = {}
+  -- for _, v in ipairs(deploymentResponse)
+  --   do
+  --   if v.state =='Failed'
+  --     then
+  --     local qf = {filename = v.filePath, lnum = v.lineNumber, col = v.columnNumber, text = v.error }
+  --     table.insert(qflist, qf)
+  --   end
+  -- end
+  -- if next(qflist) ~= nil
+  --   then
+  --   vim.fn.setqflist(qflist)
+  --   vim.api.nvim_command("botright copen")
+  -- end
+  -- handle.close()
+end
+
+
 
 local function debug(username, debug_only)
   local cmd = 'sfdx force:apex:log:tail'
